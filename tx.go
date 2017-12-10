fbfc6504b7d351486027917484f9dcfa0f41ba1a
acoshift <acoshift@gmail.com>
2017-12-10 17:26:44 +0700

    add tx funcs

---
+++ tx.go
@@ -0,0 +1,63 @@
+package pgsql
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/lib/pq"
+)
+
+// TxOptions is the transaction options
+type TxOptions struct {
+	sql.TxOptions
+	MaxAttempts int
+}
+
+const (
+	defaultMaxAttempts = 3
+)
+
+// Tx interface
+type Tx interface {
+	Commit() error
+	Rollback() error
+}
+
+// RunInTx runs fn inside retryable transaction
+func RunInTx(db *sql.DB, opts *TxOptions, fn func(*sql.Tx) error) error {
+	return RunInTxContext(context.Background(), db, opts, fn)
+}
+
+// RunInTxContext runs fn inside retryable transaction with context
+func RunInTxContext(ctx context.Context, db *sql.DB, opts *TxOptions, fn func(*sql.Tx) error) (err error) {
+	if opts == nil {
+		opts = &TxOptions{
+			MaxAttempts: defaultMaxAttempts,
+		}
+	}
+	// override default isolation level to serializable
+	if opts.Isolation == sql.LevelDefault {
+		opts.Isolation = sql.LevelSerializable
+	}
+
+	var tx *sql.Tx
+	for i := 0; i < opts.MaxAttempts; i++ {
+		tx, err = db.BeginTx(ctx, &opts.TxOptions)
+		if err != nil {
+			return
+		}
+
+		err = fn(tx)
+		if err == nil {
+			if err = tx.Commit(); err == nil {
+				return
+			}
+		}
+		pqErr, ok := err.(*pq.Error)
+		if retryable := ok && (pqErr.Code == "40001"); !retryable {
+			tx.Rollback() // ignore rollback error
+			return err
+		}
+	}
+	return
+}
---
+++ tx_test.go
@@ -0,0 +1,147 @@
+package pgsql_test
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/acoshift/pgsql"
+)
+
+func TestTx(t *testing.T) {
+	db, err := sql.Open("postgres", "postgres://postgres@localhost:5432/postgres?sslmode=disable")
+	if err != nil {
+		t.Fatalf("open database connection error; %v", err)
+	}
+	_, err = db.Exec(`
+		drop table if exists test_pgsql_tx;
+		create table test_pgsql_tx (
+			id int primary key,
+			value int
+		);
+		insert into test_pgsql_tx (
+			id, value
+		) values
+			(0, 0),
+			(1, 0),
+			(2, 0);
+	`)
+	if err != nil {
+		t.Fatalf("prepare table error; %v", err)
+	}
+	defer db.Exec(`drop table test_pgsql_tx`)
+	db.SetMaxOpenConns(30)
+
+	opts := &pgsql.TxOptions{MaxAttempts: 10}
+
+	deposit := func(balance int) error {
+		return pgsql.RunInTx(db, opts, func(tx *sql.Tx) error {
+			// log.Println("deposit", balance)
+			var acc0, acc1 int
+			err = tx.QueryRow(`select value from test_pgsql_tx where id = 0`).Scan(&acc0)
+			if err != nil {
+				return err
+			}
+			err = tx.QueryRow(`select value from test_pgsql_tx where id = 1`).Scan(&acc1)
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(`update test_pgsql_tx set value = $1 where id = 0`, acc0-balance)
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(`update test_pgsql_tx set value = $1 where id = 1`, acc1+balance)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+	withdraw := func(balance int) error {
+		return pgsql.RunInTx(db, opts, func(tx *sql.Tx) error {
+			// log.Println("withdraw", balance)
+			var acc0, acc1 int
+			err = tx.QueryRow(`select value from test_pgsql_tx where id = 1`).Scan(&acc1)
+			if err != nil {
+				return err
+			}
+			if acc1 < balance {
+				return fmt.Errorf("not enough balance to withdraw")
+			}
+			err = tx.QueryRow(`select value from test_pgsql_tx where id = 0`).Scan(&acc0)
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(`update test_pgsql_tx set value = $1 where id = 0`, acc0+balance)
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(`update test_pgsql_tx set value = $1 where id = 1`, acc1-balance)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+	transfer := func(balance int) error {
+		return pgsql.RunInTx(db, opts, func(tx *sql.Tx) error {
+			// log.Println("transfer", balance)
+			var acc1, acc2 int
+			err = tx.QueryRow(`select value from test_pgsql_tx where id = 1`).Scan(&acc1)
+			if err != nil {
+				return err
+			}
+			if acc1 < balance {
+				return fmt.Errorf("not enough balance to transfer")
+			}
+			err = tx.QueryRow(`select value from test_pgsql_tx where id = 2`).Scan(&acc2)
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(`update test_pgsql_tx set value = $1 where id = 1`, acc1-balance)
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(`update test_pgsql_tx set value = $1 where id = 2`, acc2+balance)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+
+	wg := sync.WaitGroup{}
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			var err error
+			k := rand.Intn(3)
+			if k == 0 {
+				err = deposit(rand.Intn(100000))
+			} else if k == 1 {
+				err = withdraw(rand.Intn(100000))
+			} else {
+				err = transfer(rand.Intn(100000))
+			}
+			if err != nil {
+				log.Println(err)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	var result int
+	err = db.QueryRow(`select sum(value) from test_pgsql_tx`).Scan(&result)
+	if err != nil {
+		t.Fatalf("query result error; %v", err)
+	}
+	if result != 0 {
+		t.Fatalf("expected sum all value to be 0; got %d", result)
+	}
+}
