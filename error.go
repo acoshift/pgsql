76beaffb117c370087a4ccf01e9595b7f4793b8d
acoshift <acoshift@gmail.com>
2018-01-30 21:39:36 +0700

    error: accept slice of constraint

--- error.go
+++ error.go
@@ -1,15 +1,26 @@
 package pgsql
 
-import "github.com/lib/pq"
+import (
+	"github.com/lib/pq"
+)
+
+func contains(xs []string, x string) bool {
+	for _, p := range xs {
+		if p == x {
+			return true
+		}
+	}
+	return false
+}
 
 // IsUniqueViolation checks is error unique_violation with given constraint,
 // constraint can be empty to ignore constraint name checks
-func IsUniqueViolation(err error, constraint string) bool {
+func IsUniqueViolation(err error, constraint ...string) bool {
 	if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
 		if len(constraint) == 0 {
 			return true
 		}
-		return pqErr.Constraint == constraint
+		return contains(constraint, pqErr.Constraint)
 	}
 	return false
 }
