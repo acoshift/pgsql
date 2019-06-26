0dfc327051d4ff8663fedcf2447e44a36c7a17c4
acoshift <acoshift@gmail.com>
2019-06-26 07:07:41 +0700

    pgstmt: fix result

--- pgstmt/build.go
+++ pgstmt/build.go
@@ -23,39 +23,39 @@ func (r *Result) SQL() (query string, args interface{}) {
 }
 
 func (r *Result) QueryRow(f func(string, ...interface{}) *sql.Row) *sql.Row {
-	return f(r.query, r.args)
+	return f(r.query, r.args...)
 }
 
 func (r *Result) Query(f func(string, ...interface{}) (*sql.Rows, error)) (*sql.Rows, error) {
-	return f(r.query, r.args)
+	return f(r.query, r.args...)
 }
 
 func (r *Result) Exec(f func(string, ...interface{}) (sql.Result, error)) (sql.Result, error) {
-	return f(r.query, r.args)
+	return f(r.query, r.args...)
 }
 
 func (r *Result) QueryRowContext(ctx context.Context, f func(context.Context, string, ...interface{}) *sql.Row) *sql.Row {
-	return f(ctx, r.query, r.args)
+	return f(ctx, r.query, r.args...)
 }
 
 func (r *Result) QueryContext(ctx context.Context, f func(context.Context, string, ...interface{}) (*sql.Rows, error)) (*sql.Rows, error) {
-	return f(ctx, r.query, r.args)
+	return f(ctx, r.query, r.args...)
 }
 
 func (r *Result) ExecContext(ctx context.Context, f func(context.Context, string, ...interface{}) (sql.Result, error)) (sql.Result, error) {
-	return f(ctx, r.query, r.args)
+	return f(ctx, r.query, r.args...)
 }
 
 func (r *Result) QueryRowWith(ctx context.Context) *sql.Row {
-	return pgctx.QueryRow(ctx, r.query, r.args)
+	return pgctx.QueryRow(ctx, r.query, r.args...)
 }
 
 func (r *Result) QueryWith(ctx context.Context) (*sql.Rows, error) {
-	return pgctx.Query(ctx, r.query, r.args)
+	return pgctx.Query(ctx, r.query, r.args...)
 }
 
 func (r *Result) ExecWith(ctx context.Context) (sql.Result, error) {
-	return pgctx.Exec(ctx, r.query, r.args)
+	return pgctx.Exec(ctx, r.query, r.args...)
 }
 
 type extractor interface {
