bcdf2e59dd1894101e833e951132231fcd69b275
Thanatat Tamtan <acoshift@gmail.com>
2019-09-15 08:37:03 +0700

    pgmodel: expose filter func

--- pgmodel/filter.go
+++ pgmodel/filter.go
@@ -10,9 +10,9 @@ type Filter interface {
 	Apply(ctx context.Context, b pgstmt.SelectStatement)
 }
 
-type filterFunc func(b pgstmt.SelectStatement)
+type FilterFunc func(ctx context.Context, b pgstmt.SelectStatement)
 
-func (f filterFunc) Apply(_ context.Context, b pgstmt.SelectStatement) { f(b) }
+func (f FilterFunc) Apply(ctx context.Context, b pgstmt.SelectStatement) { f(ctx, b) }
 
 func Equal(field string, value interface{}) Filter {
 	return Where(func(b pgstmt.Cond) {
@@ -21,31 +21,31 @@ func Equal(field string, value interface{}) Filter {
 }
 
 func Where(f func(b pgstmt.Cond)) Filter {
-	return filterFunc(func(b pgstmt.SelectStatement) {
+	return FilterFunc(func(_ context.Context, b pgstmt.SelectStatement) {
 		b.Where(f)
 	})
 }
 
 func Having(f func(b pgstmt.Cond)) Filter {
-	return filterFunc(func(b pgstmt.SelectStatement) {
+	return FilterFunc(func(_ context.Context, b pgstmt.SelectStatement) {
 		b.Having(f)
 	})
 }
 
 func OrderBy(col string) Filter {
-	return filterFunc(func(b pgstmt.SelectStatement) {
+	return FilterFunc(func(_ context.Context, b pgstmt.SelectStatement) {
 		b.OrderBy(col)
 	})
 }
 
 func Limit(n int64) Filter {
-	return filterFunc(func(b pgstmt.SelectStatement) {
+	return FilterFunc(func(_ context.Context, b pgstmt.SelectStatement) {
 		b.Limit(n)
 	})
 }
 
 func Offset(n int64) Filter {
-	return filterFunc(func(b pgstmt.SelectStatement) {
+	return FilterFunc(func(_ context.Context, b pgstmt.SelectStatement) {
 		b.Offset(n)
 	})
 }
