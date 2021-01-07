a90a4a1a552ae96ed5ebc7b9e59006a4cc46a000
Thanatat Tamtan <acoshift@gmail.com>
2021-01-07 13:29:50 +0700

    pgstmt: refactor test

--- pgstmt/select_test.go
+++ pgstmt/select_test.go
@@ -12,59 +12,57 @@ import (
 func TestSelect(t *testing.T) {
 	t.Parallel()
 
-	t.Run("only select", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("1")
-		}).SQL()
-
-		assert.Equal(t,
+	cases := []struct {
+		name   string
+		result *pgstmt.Result
+		query  string
+		args   []interface{}
+	}{
+		{
+			"only select",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("1")
+			}),
 			"select 1",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select from", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("id", "name")
-			b.From("users")
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"select from",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("id", "name")
+				b.From("users")
+			}),
 			"select id, name from users",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select from select", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.FromSelect(func(b pgstmt.SelectStatement) {
-				b.Columns("p.id", "p.name")
-				b.ColumnSelect(func(b pgstmt.SelectStatement) {
-					b.Columns(stripSpace(`
+			nil,
+		},
+		{
+			"select from select",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.FromSelect(func(b pgstmt.SelectStatement) {
+					b.Columns("p.id", "p.name")
+					b.ColumnSelect(func(b pgstmt.SelectStatement) {
+						b.Columns(stripSpace(`
 						json_build_object('content', coalesce(m.content, ''),
 										  'type', coalesce(m.type, 0),
 										  'timestamp', m.created_at)
 					`))
-					b.From("messages m")
-					b.Where(func(b pgstmt.Cond) {
-						b.EqRaw("m.id", "p.id")
+						b.From("messages m")
+						b.Where(func(b pgstmt.Cond) {
+							b.EqRaw("m.id", "p.id")
+						})
+						b.OrderBy("created_at").Desc().NullsFirst()
+						b.Limit(1)
+						b.Offset(2)
+					}, "msg")
+					b.From("profile p")
+					b.LeftJoin("noti n").On(func(b pgstmt.Cond) {
+						b.EqRaw("n.id", "p.id")
+						b.Eq("n.user_id", 1)
 					})
-					b.OrderBy("created_at").Desc().NullsFirst()
-					b.Limit(1)
-					b.Offset(2)
-				}, "msg")
-				b.From("profile p")
-				b.LeftJoin("noti n").On(func(b pgstmt.Cond) {
-					b.EqRaw("n.id", "p.id")
-					b.Eq("n.user_id", 1)
-				})
-			}, "t")
-		}).SQL()
-		assert.Equal(t,
-			stripSpace(`
+				}, "t")
+			}),
+			`
 				select *
 				from (select p.id, p.name, (select json_build_object('content', coalesce(m.content, ''),
 																	 'type', coalesce(m.type, 0),
@@ -76,39 +74,29 @@ func TestSelect(t *testing.T) {
 											offset 2) msg
 					  from profile p
 					  left join noti n on (n.id = p.id and n.user_id = $1)) t
-			`),
-			q,
-		)
-		assert.EqualValues(t,
+			`,
 			[]interface{}{
 				1,
 			},
-			args,
-		)
-	})
-
-	t.Run("select from where", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("id", "name")
-			b.From("users")
-			b.Where(func(b pgstmt.Cond) {
-				b.Eq("id", 3)
-				b.Eq("name", "test")
-				b.And(func(b pgstmt.Cond) {
-					b.Eq("age", 15)
-					b.Or(func(b pgstmt.Cond) {
-						b.Eq("age", 18)
+		},
+		{
+			"select from where",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("id", "name")
+				b.From("users")
+				b.Where(func(b pgstmt.Cond) {
+					b.Eq("id", 3)
+					b.Eq("name", "test")
+					b.And(func(b pgstmt.Cond) {
+						b.Eq("age", 15)
+						b.Or(func(b pgstmt.Cond) {
+							b.Eq("age", 18)
+						})
 					})
+					b.Eq("is_active", true)
 				})
-				b.Eq("is_active", true)
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			}),
 			"select id, name from users where (id = $1 and name = $2 and is_active = $3) and ((age = $4) or (age = $5))",
-			q,
-		)
-		assert.EqualValues(t,
 			[]interface{}{
 				3,
 				"test",
@@ -116,373 +104,289 @@ func TestSelect(t *testing.T) {
 				15,
 				18,
 			},
-			args,
-		)
-	})
-
-	t.Run("select from where order", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("id", "name")
-			b.From("users")
-			b.Where(func(b pgstmt.Cond) {
-				b.Eq("id", 1)
-			})
-			b.OrderBy("created_at").Asc().NullsLast()
-			b.OrderBy("id").Desc()
-		}).SQL()
-
-		assert.Equal(t,
+		},
+		{
+			"select from where order",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("id", "name")
+				b.From("users")
+				b.Where(func(b pgstmt.Cond) {
+					b.Eq("id", 1)
+				})
+				b.OrderBy("created_at").Asc().NullsLast()
+				b.OrderBy("id").Desc()
+			}),
 			"select id, name from users where (id = $1) order by created_at asc nulls last, id desc",
-			q,
-		)
-		assert.EqualValues(t,
 			[]interface{}{
 				1,
 			},
-			args,
-		)
-	})
-
-	t.Run("select limit offset", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("id", "name")
-			b.From("users")
-			b.Where(func(b pgstmt.Cond) {
-				b.Eq("id", 1)
-			})
-			b.OrderBy("id")
-			b.Limit(5)
-			b.Offset(10)
-		}).SQL()
-
-		assert.Equal(t,
+		},
+		{
+			"select limit offset",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("id", "name")
+				b.From("users")
+				b.Where(func(b pgstmt.Cond) {
+					b.Eq("id", 1)
+				})
+				b.OrderBy("id")
+				b.Limit(5)
+				b.Offset(10)
+			}),
 			"select id, name from users where (id = $1) order by id limit 5 offset 10",
-			q,
-		)
-		assert.EqualValues(t,
 			[]interface{}{
 				1,
 			},
-			args,
-		)
-	})
-
-	t.Run("join", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("id", "name")
-			b.From("users")
-			b.LeftJoin("roles using id")
-		}).SQL()
-
-		assert.Equal(t,
+		},
+		{
+			"join",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("id", "name")
+				b.From("users")
+				b.LeftJoin("roles using id")
+			}),
 			"select id, name from users left join roles using id",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("join on", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("id", "name")
-			b.From("users")
-			b.LeftJoin("roles").On(func(b pgstmt.Cond) {
-				b.EqRaw("users.id", "roles.id")
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"join on",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("id", "name")
+				b.From("users")
+				b.LeftJoin("roles").On(func(b pgstmt.Cond) {
+					b.EqRaw("users.id", "roles.id")
+				})
+			}),
 			"select id, name from users left join roles on (users.id = roles.id)",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("join using", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("id", "name")
-			b.From("users")
-			b.InnerJoin("roles").Using("id", "name")
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"join using",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("id", "name")
+				b.From("users")
+				b.InnerJoin("roles").Using("id", "name")
+			}),
 			"select id, name from users inner join roles using (id, name)",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("join select", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("id", "name", "count(*)")
-			b.From("users")
-			b.LeftJoinSelect(func(b pgstmt.SelectStatement) {
-				b.Columns("user_id", "data")
-				b.From("event")
-			}, "t").On(func(b pgstmt.Cond) {
-				b.EqRaw("t.user_id", "users.id")
-			})
-			b.GroupBy("id", "name")
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"join select",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("id", "name", "count(*)")
+				b.From("users")
+				b.LeftJoinSelect(func(b pgstmt.SelectStatement) {
+					b.Columns("user_id", "data")
+					b.From("event")
+				}, "t").On(func(b pgstmt.Cond) {
+					b.EqRaw("t.user_id", "users.id")
+				})
+				b.GroupBy("id", "name")
+			}),
 			"select id, name, count(*) from users left join (select user_id, data from event) t on (t.user_id = users.id) group by (id, name)",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("group by having", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("city", "max(temp_lo)")
-			b.From("weather")
-			b.GroupBy("city")
-			b.Having(func(b pgstmt.Cond) {
-				b.LtRaw("max(temp_lo)", 40)
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"group by having",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("city", "max(temp_lo)")
+				b.From("weather")
+				b.GroupBy("city")
+				b.Having(func(b pgstmt.Cond) {
+					b.LtRaw("max(temp_lo)", 40)
+				})
+			}),
 			"select city, max(temp_lo) from weather group by (city) having (max(temp_lo) < 40)",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select any", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.Eq("x", pgstmt.Any(pq.Array([]int64{1, 2})))
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"select any",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.Eq("x", pgstmt.Any(pq.Array([]int64{1, 2})))
+				})
+			}),
 			"select * from table where (x = any($1))",
-			q,
-		)
-		assert.EqualValues(t,
 			[]interface{}{
 				pq.Array([]int64{1, 2}),
 			},
-			args,
-		)
-	})
-
-	t.Run("select in", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.In("x", 1, 2)
-			})
-		}).SQL()
-
-		assert.Equal(t,
+		},
+		{
+			"select in",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.In("x", 1, 2)
+				})
+			}),
 			"select * from table where (x in ($1, $2))",
-			q,
-		)
-		assert.EqualValues(t,
 			[]interface{}{
 				1,
 				2,
 			},
-			args,
-		)
-	})
-
-	t.Run("select in select", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.InSelect("id", func(b pgstmt.SelectStatement) {
-					b.Columns("id")
-					b.From("table2")
+		},
+		{
+			"select in select",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.InSelect("id", func(b pgstmt.SelectStatement) {
+						b.Columns("id")
+						b.From("table2")
+					})
 				})
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			}),
 			"select * from table where (id in (select id from table2))",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select not in", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.NotIn("x", 1, 2)
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"select not in",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.NotIn("x", 1, 2)
+				})
+			}),
 			"select * from table where (x not in ($1, $2))",
-			q,
-		)
-		assert.EqualValues(t,
 			[]interface{}{
 				1,
 				2,
 			},
-			args,
-		)
-	})
-
-	t.Run("select and mode", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.Mode().And()
-				b.EqRaw("a", 1)
-				b.EqRaw("a", 2)
-			})
-		}).SQL()
-
-		assert.Equal(t,
+		},
+		{
+			"select and mode",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.Mode().And()
+					b.EqRaw("a", 1)
+					b.EqRaw("a", 2)
+				})
+			}),
 			"select * from table where (a = 1 and a = 2)",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select or mode", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.Mode().Or()
-				b.EqRaw("a", 1)
-				b.EqRaw("a", 2)
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"select or mode",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.Mode().Or()
+					b.EqRaw("a", 1)
+					b.EqRaw("a", 2)
+				})
+			}),
 			"select * from table where (a = 1 or a = 2)",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select nested or mode", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.EqRaw("a", 1)
-				b.And(func(b pgstmt.Cond) {
-					b.Mode().Or()
-					b.EqRaw("a", 2)
-					b.EqRaw("a", 3)
-				})
-			})
-		}).SQL()
-
-		assert.Equal(t,
-			"select * from table where (a = 1) and (a = 2 or a = 3)",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select nested and", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.EqRaw("a", 1)
-				b.EqRaw("b", 1)
-				b.And(func(b pgstmt.Cond) {
+			nil,
+		},
+		{
+			"select nested or mode",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.EqRaw("a", 1)
 					b.And(func(b pgstmt.Cond) {
-						b.EqRaw("c", 1)
-						b.EqRaw("d", 1)
-					})
-					b.Or(func(b pgstmt.Cond) {
-						b.EqRaw("e", 1)
-						b.EqRaw("f", 1)
+						b.Mode().Or()
+						b.EqRaw("a", 2)
+						b.EqRaw("a", 3)
 					})
 				})
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			}),
+			"select * from table where (a = 1) and (a = 2 or a = 3)",
+			nil,
+		},
+		{
+			"select nested and",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.EqRaw("a", 1)
+					b.EqRaw("b", 1)
+					b.And(func(b pgstmt.Cond) {
+						b.And(func(b pgstmt.Cond) {
+							b.EqRaw("c", 1)
+							b.EqRaw("d", 1)
+						})
+						b.Or(func(b pgstmt.Cond) {
+							b.EqRaw("e", 1)
+							b.EqRaw("f", 1)
+						})
+					})
+				})
+			}),
 			"select * from table where (a = 1 and b = 1) and ((c = 1 and d = 1) or (e = 1 and f = 1))",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select nested and single or without ops", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.EqRaw("a", 1)
-				b.EqRaw("b", 1)
-				b.And(func(b pgstmt.Cond) {
-					// nothing to `or` with
-					b.Or(func(b pgstmt.Cond) {
-						b.EqRaw("c", 1)
-						b.EqRaw("d", 1)
+			nil,
+		},
+		{
+			"select nested and single or without ops",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.EqRaw("a", 1)
+					b.EqRaw("b", 1)
+					b.And(func(b pgstmt.Cond) {
+						// nothing to `or` with
+						b.Or(func(b pgstmt.Cond) {
+							b.EqRaw("c", 1)
+							b.EqRaw("d", 1)
+						})
 					})
 				})
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			}),
 			"select * from table where (a = 1 and b = 1) and (c = 1 and d = 1)",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select without op but nested", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Columns("*")
-			b.From("table")
-			b.Where(func(b pgstmt.Cond) {
-				b.And(func(b pgstmt.Cond) {
-					b.Mode().Or()
-					b.EqRaw("a", 2)
-					b.EqRaw("a", 3)
+			nil,
+		},
+		{
+			"select without op but nested",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Columns("*")
+				b.From("table")
+				b.Where(func(b pgstmt.Cond) {
+					b.And(func(b pgstmt.Cond) {
+						b.Mode().Or()
+						b.EqRaw("a", 2)
+						b.EqRaw("a", 3)
+					})
 				})
-			})
-		}).SQL()
-
-		assert.Equal(t,
+			}),
 			"select * from table where (a = 2 or a = 3)",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select distinct", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Distinct()
-			b.Columns("col_1")
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"select distinct",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Distinct()
+				b.Columns("col_1")
+			}),
 			"select distinct col_1",
-			q,
-		)
-		assert.Empty(t, args)
-	})
-
-	t.Run("select distinct on", func(t *testing.T) {
-		q, args := pgstmt.Select(func(b pgstmt.SelectStatement) {
-			b.Distinct().On("col_1", "col_2")
-			b.Columns("col_1", "col_3")
-		}).SQL()
-
-		assert.Equal(t,
+			nil,
+		},
+		{
+			"select distinct on",
+			pgstmt.Select(func(b pgstmt.SelectStatement) {
+				b.Distinct().On("col_1", "col_2")
+				b.Columns("col_1", "col_3")
+			}),
 			"select distinct on (col_1, col_2) col_1, col_3",
-			q,
-		)
-		assert.Empty(t, args)
-	})
+			nil,
+		},
+	}
+
+	for _, tC := range cases {
+		t.Run(tC.name, func(t *testing.T) {
+			q, args := tC.result.SQL()
+			assert.Equal(t, stripSpace(tC.query), q)
+			assert.EqualValues(t, tC.args, args)
+		})
+	}
 }
